ed3658f11837d80137bf1224443b6ca41f55a720
Victor <enterprevic@gmail.com>
2023-10-28 19:43:49 +0300

    simple BMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import Slider from "@mui/material-next/Slider";
+import { AiOutlineMan, AiOutlineWoman } from "react-icons/ai";
 
 function App() {
+  const [weight, setWeight] = React.useState(0);
+  const [height, setHeight] = React.useState(0);
+  const [BMIStatus, setBMIStatus] = React.useState("Body Mass Index");
+
+  const handleWeightChange = (event, newWeight) => {
+    setWeight(newWeight);
+    BMICalc(newWeight, height);
+  };
+
+  const handleHeightChange = (event, newHeight) => {
+    setHeight(newHeight);
+    BMICalc(weight, newHeight);
+  };
+
+  const BMICalc = (newWeight, newHeight) => {
+    const BMI = newWeight / (newHeight / 100) ** 2;
+    if (BMI <= 18.5) setBMIStatus("Underweight");
+    else if (BMI >= 18.6 && BMI < 24.9) setBMIStatus("Normal Weight");
+    else if (BMI >= 25 && BMI < 29.9) setBMIStatus("Overweight");
+    else setBMIStatus("Obesity");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      BMI Calculator
+      <div className="gender">
+        Gender:
+        <button>
+          Male
+          <AiOutlineMan />
+        </button>
+        <button>
+          Female
+          <AiOutlineWoman />
+        </button>
+      </div>
+      <div className="sliderBar">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Weight(kg):
+          <Slider
+            aria-label="Weight"
+            defaultValue={80}
+            onChange={handleWeightChange}
+            max={150}
+            step={1}
+            valueLabelDisplay="auto"
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="sliderBar">
+        <p>
+          Height(cm):
+          <Slider
+            aria-label="Height"
+            defaultValue={80}
+            onChange={handleHeightChange}
+            max={220}
+            step={1}
+            valueLabelDisplay="auto"
+          />
+        </p>
+      </div>
+      <p>BMI: {BMIStatus}</p>
     </div>
   );
 }
